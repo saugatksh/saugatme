7b00f76dfeb638938c3ba11e68addede272ce6d2
saugatksh <69414867+saugatksh@users.noreply.github.com>
2024-08-06 10:34:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Editor", "Singer"],
+        strings: ["Odoo, Python and Django Developer","Singer", "Guitarist", "Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Editor", "Singer"],
+        strings: ["Odoo, Python and Django Developer","Singer", "Guitarist", "Video Editor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
